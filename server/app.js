59edbc26b38f1be708ab0a7d0302c01d6c24dda2
josearangos <jose.arangos@udea.edu.co>
2018-02-26 14:15:01 -0500

    conexion a mongo  y creacion de pos y get

--- server/app.js
+++ server/app.js
@@ -1,11 +1,53 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-
+const port=4100;
+const dbMongo='mongodb://localhost:27017/bdStory';
+const mongoose = require('mongoose');
+const Word=require('./Model/word');
 var storyParts = [];
 
-app.use(express.static('public'));
+mongoose.connect(dbMongo, (err, res) => {
+    if (err) {
+        return console.log(`Error al conectarse a la base de datos: ${err}`);
+    } else {
+        console.log("conecion establecida");
+    }
+    app.listen(port, () => {
+        console.log(`API corriendo por el puerto: ${port}`);
+    })
+});
+
+// ingresar palabra a la bd
+app.post('/api/word',(req,res)=>{
+	let word = new Word();
+	word.word=req.body.word;
+	word.save((err, wordStored) => {
+        if (err) {
+            res.status(500) // los 500 son errores del Servidor
+            res.send({ message: `Erro al salvar en la BD ${err}` });
+        } else {
+            res.status(200);
+            res.send({ word: wordStored });
+        }
+    });
+});
+
+app.get('/api/words', (req, res) => {
+    // con el {} trae todas las palabras
+    Word.find({}, (err, words) => {
+        if (err) return res.status(500).send({ message: `Error al buscar ${err}` });
+        if (!words) return res.status(404).send({ message: `No hay palabras` });
+        res.status(200).send({ words: words });
+    });
+});
+
+
 
 io.on('connection', function(socket){
 	console.log("se conecto alguien con socket");
@@ -17,6 +59,5 @@ io.on('connection', function(socket){
 	})
 });
 
-server.listen(8080, function(){
-	console.log("Corriendo por el puerto 8080")
-});
+
+
